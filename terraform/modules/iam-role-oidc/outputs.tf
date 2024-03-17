b7aaf3716b71906cfaa2d98c13d193beca754a89
Kamil Herbik (khdevel) <kamil.herbik@10clouds.com>
2024-03-17 20:57:07 +0100

    [main]

--- terraform/modules/iam-role-oidc/outputs.tf
+++ terraform/modules/iam-role-oidc/outputs.tf
@@ -1,9 +1,9 @@
 output "iam_role_arn" {
   description = "The ARN of the IAM role"
-  value       = aws_iam_role.this[0].arn
+  value       = try(aws_iam_role.this[0].arn, null)
 }
 
 output "iam_role_name" {
   description = "The name of the IAM role"
-  value       = aws_iam_role.this[0].name
+  value       = try(aws_iam_role.this[0].name, null)
 }
